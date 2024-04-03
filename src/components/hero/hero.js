f3b8db0768691e112e179d1cc5384ac3faae30ea
Meiscool125 <dathan.kenworthy@gmail.com>
2024-04-03 18:00:21 -0500

    updated hero
    Co-authored-by: UsmanS2 <UsmanS2@users.noreply.github.com>

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -6,9 +6,18 @@ import BlueGeoImage from '../../assets/blue-geometric.png'
 const Hero = () => {
     return (
       <div className="heroContainer">
-          <img src={BlueGeoImage} alt='blue geo' className='geoImg'></img>
+          <div className ="geoImgContainer"><img src={BlueGeoImage} alt='blue geo' className='geoImg'></img></div>
+          <div className='mainText'>
+            <div className ="mainTextContainer1"><h2>Hello, I'm Dathan.</h2></div>
+            <div className ="mainTextContainer2"><h2>I'm a high schooler who's into coding.</h2></div>
+            <div className ="mainTextContainer3"><h2>I try to use coding to solve tasks.</h2></div>
+          </div>
+          <div className='navLinks'>
+          <a href="https://github.com/Meiscool125">My Github</a>
+          <a href="">About Me</a>
+          <a href="">Projects</a>
+          </div>
       </div>
     );
   };
-  
   export default Hero;

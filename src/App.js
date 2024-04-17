78f64db1c8b308d7a50a290a669cbeb05ca08e31
Meiscool125 <dathan.kenworthy@gmail.com>
2024-04-17 17:06:29 -0500

    made about me section
    Co-authored-by: UsmanS2 <UsmanS2@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/nav-bar/nav-bar';
 import Hero from './components/hero/hero';
+import AboutMe from './components/aboutMe/aboutMe';
+
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <AboutMe />
     </div>
 
   );
--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -4,17 +4,18 @@ import './hero.css'; // Import the CSS file
 import BlueGeoImage from '../../assets/blue-geometric.png'
 
 const Hero = () => {
+    const [activeNav, setActiveNav] = React.useState("#")
     return (
       <div className="heroContainer">
           <div className ="geoImgContainer"><img src={BlueGeoImage} alt='blue geo' className='geoImg'></img></div>
           <div className='mainText'>
             <div className ="mainTextContainer1"><h2>Hello, I'm Dathan.</h2></div>
             <div className ="mainTextContainer2"><h2>I'm a high schooler who's into coding.</h2></div>
-            <div className ="mainTextContainer3"><h2>I try to use coding to solve tasks.</h2></div>
+            <div className ="mainTextContainer3"><h2>I use coding to make fun projects.</h2></div>
           </div>
           <div className='navLinks'>
           <a href="https://github.com/Meiscool125">My Github</a>
-          <a href="">About Me</a>
+          <a href="#about" onClick={()=>setActiveNav("#about")}>About Me</a>
           <a href="">Projects</a>
           </div>
       </div>
